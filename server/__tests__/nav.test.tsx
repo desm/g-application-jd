ebd5dd7303a503d023cec4c943ebe3c2e08f7eae
Jacques Desmarais <jdesma@gmail.com>
2024-02-29 00:08:06 -0500

    fixes the background image of /signup and the avatar image in the Nav panel

--- server/__tests__/nav.test.tsx
+++ server/__tests__/nav.test.tsx
@@ -5,7 +5,7 @@ import Nav from '../app/javascript/bundles/Dashboard/components/Nav';
 
 describe('Nav', () => {
   it('should contain the navbar element "Home"', async () => {
-    render(<Nav />);
+    render(<Nav avatar_img="/xyz" />);
     const firstSection = document.getElementsByTagName('section')[0];
     const homeLink = within(firstSection).getByText('Home')
     expect(homeLink).toBeInstanceOf(HTMLAnchorElement);
