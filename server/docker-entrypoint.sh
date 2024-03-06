83ec7fe2aa32fa4ae81fd615014436b9e84ed7a6
Jacques Desmarais <jdesma@gmail.com>
2024-03-06 18:34:03 -0500

    successfully deploys gumroad-like app to both "staging" and "production"

--- server/docker-entrypoint.sh
+++ server/docker-entrypoint.sh
@@ -3,8 +3,8 @@
 # If running the rails server then create or migrate existing database
 if [ "${1}" == "./bin/rails" ] && [ "${2}" == "server" ]; then
     echo "RAILS_ENV is $RAILS_ENV"
-    echo "running \"rails db:version\" BEFORE db:prepare"
-    ./bin/rails db:version
+    echo "running \"rails db:version || rails db:create\" BEFORE db:prepare"
+    ./bin/rails db:version || ./bin/rails db:create
     ./bin/rails db:prepare
     echo "running \"rails db:version\" AFTER db:prepare"
     ./bin/rails db:version
