6de28a246a16f49a6d85d6082513d2896c70cca7
Jacques Desmarais <jdesma@gmail.com>
2024-03-18 21:20:35 -0400

    restores correct copy of docker-entrypoint.sh (more)
    reason: previous commit changed it in order to completely rebuild databases in staging and production environments

--- server/production/docker-entrypoint.sh
+++ server/production/docker-entrypoint.sh
@@ -4,16 +4,16 @@ echo "RAILS_ENV is $RAILS_ENV"
 
 # If running the rails server then create or migrate existing database
 if [ "${1}" == "./bin/rails" ] && [ "${2}" == "server" ]; then
-    # echo "running \"rails db:version || rails db:create\" BEFORE db:prepare"
-    # ./bin/rails db:version || ./bin/rails db:create
-    # ./bin/rails db:prepare
-    # echo "running \"rails db:version\" AFTER db:prepare"
-    # ./bin/rails db:version
+    echo "running \"rails db:version || rails db:create\" BEFORE db:prepare"
+    ./bin/rails db:version || ./bin/rails db:create
+    ./bin/rails db:prepare
+    echo "running \"rails db:version\" AFTER db:prepare"
+    ./bin/rails db:version
 
-    echo "running drop, create, migrate"
-    ./bin/rails db:drop
-    ./bin/rails db:create
-    ./bin/rails db:migrate
+    # echo "running drop, create, migrate"
+    # ./bin/rails db:drop
+    # ./bin/rails db:create
+    # ./bin/rails db:migrate
 fi
 
 # Dropbear SSH server - never ended up needing
