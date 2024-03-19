ebf995102c8841f4e6d4c8e9a67ca8d57b950646
Jacques Desmarais <jdesma@gmail.com>
2024-03-19 17:19:40 -0400

    restores docker-entrypoint.sh to only run db:prepare, instead of dropping, creating, migrating which was a temporary measure

--- server/production/docker-entrypoint.sh
+++ server/production/docker-entrypoint.sh
@@ -4,16 +4,16 @@ echo "RAILS_ENV is $RAILS_ENV"
 
 # If running the rails server then create or migrate existing database
 if [ "${1}" == "./bin/rails" ] && [ "${2}" == "server" ]; then
-    # echo "running \"rails db:version || rails db:create\" BEFORE db:prepare"
-    # ./bin/rails db:version || ./bin/rails db:create
-    # ./bin/rails db:prepare
-    # echo "running \"rails db:version\" AFTER db:prepare"
-    # ./bin/rails db:version
+    echo "running \"rails db:version || rails db:create\" BEFORE db:prepare"
+    ./bin/rails db:version || ./bin/rails db:create
+    ./bin/rails db:prepare
+    echo "running \"rails db:version\" AFTER db:prepare"
+    ./bin/rails db:version
 
-    echo "running drop, create, migrate"
-    DISABLE_DATABASE_ENVIRONMENT_CHECK=1 ./bin/rails db:drop
-    DISABLE_DATABASE_ENVIRONMENT_CHECK=1 ./bin/rails db:create
-    DISABLE_DATABASE_ENVIRONMENT_CHECK=1 ./bin/rails db:migrate
+    # echo "running drop, create, migrate"
+    # DISABLE_DATABASE_ENVIRONMENT_CHECK=1 ./bin/rails db:drop
+    # DISABLE_DATABASE_ENVIRONMENT_CHECK=1 ./bin/rails db:create
+    # DISABLE_DATABASE_ENVIRONMENT_CHECK=1 ./bin/rails db:migrate
 fi
 
 # Dropbear SSH server - never ended up needing
