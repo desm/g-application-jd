01237e69d1d9c32bd361d27f5769e4d9620750ee
Jacques Desmarais <jdesma@gmail.com>
2024-03-25 10:25:03 -0400

    removes some unused code to do with an old fetch test

--- webapp/client/bundles/lib.ts
+++ webapp/client/bundles/lib.ts
@@ -1,30 +1,3 @@
-export const createLink = async () => {
-  const response = await fetch('http://localhost/links', {
-    method: 'POST',
-    headers: {
-      'content-type': 'application/json',
-    },
-    body: JSON.stringify({
-      link: {
-        is_physical: false,
-        is_recurring_billing: false,
-        name: 'Hey',
-        native_type: 'digital',
-        price_currency_type: 'cad',
-        price_range: '299',
-        release_at_date: 'March 24, 2024',
-        release_at_time: '12PM',
-        subscription_duration: null,
-      },
-    }),
-    referrer: 'http://localhost/products/new',
-    referrerPolicy: 'strict-origin-when-cross-origin',
-    mode: 'cors',
-    credentials: 'include',
-  });
-  return response.json();
-};
-
 /**
  * searches document for all <div data-...="{...}" id="..." /> elements, parses the data,
  * and returns as a map { "some-id": objA, "other-id", objB, ... }
