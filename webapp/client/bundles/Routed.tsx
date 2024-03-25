8876d78280eca321ba54280efc1c2febd9d6170f
Jacques Desmarais <jdesma@gmail.com>
2024-03-25 14:35:08 -0400

    uses data API "createHashRouter"

--- webapp/client/bundles/Routed.tsx
+++ webapp/client/bundles/Routed.tsx
@@ -1,29 +1,44 @@
 import type { FunctionComponent } from 'react';
 import * as React from 'react';
-import { BrowserRouter, Route, Routes, HashRouter } from 'react-router-dom';
 import Blogs from './pages/Blogs.js';
 import Contact from './pages/Contact.js';
 import Home from './pages/Home.js';
 import Layout from './pages/Layout.js';
 import NoPage from './pages/NoPage.js';
 
+import { createHashRouter, RouterProvider } from 'react-router-dom';
+
+const router = createHashRouter([
+  {
+    path: '',
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: 'blogs',
+        element: <Blogs />,
+      },
+      {
+        path: 'contact',
+        element: <Contact />,
+      },
+      {
+        path: '*',
+        element: <NoPage />,
+      },
+    ],
+  },
+]);
+
 export interface Props {}
 
 // Note, you need to declare the `FunctionComponent` type so that it complies
 // with `ReactOnRails.register` type.
 const Routed: FunctionComponent<Props> = (props: Props) => {
-  return (
-    <HashRouter>
-      <Routes>
-        <Route path="" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="blogs" element={<Blogs />} />
-          <Route path="contact" element={<Contact />} />
-          <Route path="*" element={<NoPage />} />
-        </Route>
-      </Routes>
-    </HashRouter>
-  );
+  return <RouterProvider router={router} />;
 };
 
 export default Routed;
