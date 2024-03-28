5126bd359e206a010dc1bdb06d646eb7a7ee33f2
Jacques Desmarais <jdesma@gmail.com>
2024-03-27 21:50:23 -0400

    implements half of the menubar buttons of the rich text editor in the Content Tab

--- webapp/client/bundles/ProductContentPreview/ProductContent.tsx
+++ webapp/client/bundles/ProductContentPreview/ProductContent.tsx
@@ -6,7 +6,7 @@ import { EditorState } from 'prosemirror-state';
 import { EditorView } from 'prosemirror-view';
 import * as React from 'react';
 import { useEffect } from 'react';
-import { createMenuPlugin } from './ProductContentRichTextMenu';
+import { createMenuPluginForContentTab } from './ProductContentRichTextMenu';
 import contentTabRichTextDoc from './rtDocContentTab.json';
 import { applicationState } from './stateStores/application';
 import { changeEditorState, setEditorView } from './stateStores/textEditor';
@@ -22,7 +22,7 @@ function ProductContent() {
     const editorState = EditorState.fromJSON(
       {
         schema: mySchema,
-        plugins: [...exampleSetup({ schema: mySchema, menuBar: false }), createMenuPlugin(mySchema)],
+        plugins: [...exampleSetup({ schema: mySchema, menuBar: false }), createMenuPluginForContentTab(mySchema)],
       },
       contentTabRichTextDoc
     );
@@ -41,42 +41,266 @@ function ProductContent() {
     <>
       <main className="product-content" style={{ height: '100%' }}>
         <div role="toolbar" className="content-tab rich-text-editor-toolbar">
-          <div style={{ display: 'flex', marginLeft: 'auto' }}>
-            <div className="combobox version-dropdown" style={{ width: '300px' }}>
-              <div
-                role="combobox"
+          <span role="button" aria-pressed="false" aria-label="Bold">
+            <span className="icon icon-bold"></span>
+          </span>
+          <span role="button" aria-pressed="false" aria-label="Italic">
+            <span className="icon icon-italic"></span>
+          </span>
+          <span role="button" aria-pressed="false" aria-label="Underline" aria-disabled="true">
+            <span className="icon icon-underline editor-function-not-implemented"></span>
+          </span>
+          <span role="button" aria-pressed="false" aria-label="Strikethrough" aria-disabled="true">
+            <span className="icon icon-strikethrough editor-function-not-implemented"></span>
+          </span>
+          <details className="popover toggle">
+            <summary>
+              <span
+                role="button"
+                aria-pressed="false"
+                aria-label="Headings"
+                aria-haspopup="true"
                 aria-expanded="false"
-                aria-controls=":r1h:"
                 tabIndex={0}
-                className="input"
-                aria-label="Select a version"
               >
-                <span className="fake-input text-singleline">Editing: one</span>
-                <span className="icon icon-outline-cheveron-down"></span>
-              </div>
-              <div hidden={true}>
-                <datalist id=":r1h:" aria-multiselectable="true">
-                  <div role="option" id=":r1h:-0" className="" aria-selected="true" aria-disabled="false">
-                    <div>
-                      <h4>one</h4>
-                      <small>Editing</small>
-                    </div>
-                  </div>
-                  <div role="option" id=":r1h:-1" className="" aria-selected="false" aria-disabled="false">
-                    <div>
-                      <h4>Untitled</h4>
-                      <small className="text-muted">No content yet</small>
-                    </div>
-                  </div>
-                  <div className="option">
-                    <label>
-                      <input type="checkbox" tabIndex={-1} />
-                      <small>Use the same content for all versions</small>
-                    </label>
-                  </div>
-                </datalist>
+                <span className="icon icon-headings"></span>
+              </span>
+            </summary>
+            <div
+              className="dropdown"
+              style={{
+                transform: 'translateX(min(460px - 100% - var(--spacer-4), 0px))',
+                maxWidth: 'calc(673px - 2 * var(--spacer-4))',
+              }}
+            >
+              <ul role="menu" style={{ padding: '0px', overflowX: 'hidden' }}>
+                <li role="menuitemradio" aria-checked="false">
+                  <span className="icon icon-h1"></span>
+                  <span>Large heading</span>
+                </li>
+                <li role="menuitemradio" aria-checked="false">
+                  <span className="icon icon-h2"></span>
+                  <span>Medium heading</span>
+                </li>
+                <li role="menuitemradio" aria-checked="false">
+                  <span className="icon icon-h3"></span>
+                  <span>Small heading</span>
+                </li>
+              </ul>
+            </div>
+          </details>
+          <span role="button" aria-pressed="false" aria-label="Toggle code block">
+            <span className="icon icon-code"></span>
+          </span>
+          <span role="button" aria-pressed="false" aria-label="Bulleted list">
+            <span className="icon icon-unordered-list"></span>
+          </span>
+          <span role="button" aria-pressed="false" aria-label="Numbered list">
+            <span className="icon icon-ordered-list"></span>
+          </span>
+          <span role="button" aria-label="Horizontal line">
+            <span className="icon icon-horizontal-rule"></span>
+          </span>
+          <span role="button" aria-pressed="false" aria-label="Quote">
+            <span className="icon icon-quote"></span>
+          </span>
+          <details
+            className="popover toggle"
+            onClick={(e) => {
+              e.preventDefault();
+            }}
+          >
+            <summary>
+              <span
+                role="button"
+                aria-pressed="false"
+                aria-label="Insert link"
+                aria-haspopup="true"
+                aria-expanded="false"
+                aria-disabled="true"
+              >
+                <span className="icon icon-link editor-function-not-implemented"></span>
+              </span>
+            </summary>
+            <div
+              className="dropdown"
+              style={{
+                transform: 'translateX(min(0px - 100% - var(--spacer-4), 0px))',
+                maxWidth: 'calc(0px - 2 * var(--spacer-4))',
+              }}
+            >
+              <fieldset className="tiptap__link-popover">
+                <input className="top-level-input" type="text" placeholder="Enter text" defaultValue="" tabIndex={-1} />
+                <input className="top-level-input" type="text" placeholder="Enter URL" defaultValue="" tabIndex={-1} />
+                <button className="primary" type="button">
+                  Add link
+                </button>
+              </fieldset>
+            </div>
+          </details>
+          <div role="separator" aria-orientation="vertical"></div>
+          <label>
+            <span role="button" aria-pressed="false" aria-label="Insert image" aria-disabled="true">
+              <span className="icon icon-image editor-function-not-implemented"></span>
+            </span>
+            {/* <input multiple={false} type="file" accept=".jpeg,.jpg,.png,.gif,.webp" tabIndex={-1} /> */}
+          </label>
+          <details
+            className="popover toggle"
+            onClick={(e) => {
+              e.preventDefault();
+            }}
+          >
+            <summary>
+              <span
+                role="button"
+                aria-label="Insert button"
+                aria-haspopup="true"
+                aria-expanded="false"
+                aria-disabled="true"
+              >
+                <span className="icon icon-button editor-function-not-implemented"></span>
+              </span>
+            </summary>
+            <div
+              className="dropdown"
+              style={{
+                transform: 'translateX(min(0px - 100% - var(--spacer-4), 0px))',
+                maxWidth: 'calc(0px - 2 * var(--spacer-4))',
+              }}
+            >
+              <fieldset className="tiptap__link-popover">
+                <input className="top-level-input" type="text" placeholder="Enter text" defaultValue="" tabIndex={-1} />
+                <input className="top-level-input" type="text" placeholder="Enter URL" defaultValue="" tabIndex={-1} />
+                <button className="primary" type="button">
+                  Add button
+                </button>
+              </fieldset>
+            </div>
+          </details>
+          <details
+            className="popover toggle"
+            onClick={(e) => {
+              e.preventDefault();
+            }}
+          >
+            <summary>
+              <span
+                role="button"
+                aria-pressed="false"
+                aria-label="Insert file"
+                aria-haspopup="true"
+                aria-expanded="false"
+                aria-disabled="true"
+              >
+                <span className="icon icon-upload editor-function-not-implemented"></span>
+              </span>
+            </summary>
+            <div
+              className="dropdown"
+              style={{
+                transform: 'translateX(min(0px - 100% - var(--spacer-4), 0px))',
+                maxWidth: 'calc(0px - 2 * var(--spacer-4))',
+              }}
+            >
+              <div aria-label="Product file uploader">
+                <input type="file" name="file" multiple={false} tabIndex={-1} />
+                <div className="tab-buttons small" role="tablist">
+                  <button className="" role="tab" type="button">
+                    <span className="icon icon-upload-fill"></span>Computer files
+                  </button>
+                  <button
+                    className="disabled"
+                    role="tab"
+                    type="button"
+                    aria-selected="false"
+                    aria-controls=":r1e:"
+                    aria-disabled="true"
+                  >
+                    <span className="icon icon-files-earmark"></span>Existing product
+                  </button>
+                  <button className="" role="tab" type="button">
+                    <span className="icon icon-dropbox"></span>Dropbox import
+                  </button>
+                  <button className="" role="tab" type="button" aria-selected="false" aria-controls=":r1d:">
+                    <span className="icon icon-embed"></span>Embed video
+                  </button>
+                </div>
               </div>
             </div>
+          </details>
+          <details
+            className="popover toggle"
+            onClick={(e) => {
+              e.preventDefault();
+            }}
+          >
+            <summary>
+              <span
+                role="button"
+                aria-label="More options"
+                aria-haspopup="true"
+                aria-expanded="false"
+                aria-disabled="true"
+              >
+                <span className="icon icon-outline-dots-circle-horizontal editor-function-not-implemented"></span>
+              </span>
+            </summary>
+            <div
+              className="dropdown"
+              style={{
+                transform: 'translateX(min(0px - 100% - var(--spacer-4), 0px))',
+                maxWidth: 'calc(0px - 2 * var(--spacer-4))',
+              }}
+            >
+              <div role="menu">
+                <div role="menuitem">
+                  <span className="icon icon-file-earmark-medical"></span>
+                  <span>List of posts</span>
+                </div>
+                <div role="menuitem">
+                  <span className="icon icon-outline-key"></span>
+                  <span>License key</span>
+                </div>
+                <details className="popover toggle" style={{ display: 'block' }}>
+                  <summary>
+                    <div role="menuitem">
+                      <span className="icon icon-twitter"></span>
+                      <span>Insert tweet</span>
+                    </div>
+                  </summary>
+                  <div
+                    className="dropdown"
+                    style={{
+                      transform: 'translateX(min(0px - 100% - var(--spacer-4), 0px))',
+                      maxWidth: 'calc(0px - 2 * var(--spacer-4))',
+                    }}
+                  >
+                    <fieldset>
+                      <legend>
+                        <label htmlFor=":r1g:">Tweet URL</label>
+                      </legend>
+                      <input
+                        id=":r1g:"
+                        className="top-level-input"
+                        type="text"
+                        placeholder="https://x.com/gumroad/status/1663556902624845824"
+                        tabIndex={-1}
+                      />
+                      <button className="primary" type="button">
+                        Insert
+                      </button>
+                    </fieldset>
+                  </div>
+                </details>
+              </div>
+            </div>
+          </details>
+          <div role="separator" aria-orientation="vertical"></div>
+          <span role="button" aria-label="Create page" aria-disabled="true">
+            <span className="icon icon-file-earmark-plus editor-function-not-implemented"></span>
+          </span>{' '}
+          <div style={{ display: 'flex', marginLeft: 'auto' }}>
             <span role="button" aria-pressed="false" aria-disabled="true" aria-label="Undo last change">
               <span className="icon icon-undo"></span>
             </span>
--- webapp/client/bundles/ProductContentPreview/ProductContentRichTextMenu.ts
+++ webapp/client/bundles/ProductContentPreview/ProductContentRichTextMenu.ts
@@ -1,4 +1,6 @@
-import { toggleMark } from 'prosemirror-commands';
+import { setBlockType, toggleMark, wrapIn } from 'prosemirror-commands';
+import { redo, undo } from 'prosemirror-history';
+import { wrapInList } from 'prosemirror-schema-list';
 import { Plugin } from 'prosemirror-state';
 
 class MenuView {
@@ -11,22 +13,25 @@ class MenuView {
     this.editorView = editorView;
 
     this.dom = document.querySelector('.content-tab.rich-text-editor-toolbar') as HTMLDivElement;
-    [...this.items].reverse().forEach(({ dom }) => this.dom.insertBefore(dom, this.dom.firstChild));
     this.update();
 
     this.dom.addEventListener('mousedown', (e) => {
+      console.log('e', e);
       e.preventDefault();
       editorView.focus();
-      items.forEach(({ command, dom }) => {
-        if (dom.contains(e.target)) command(editorView.state, editorView.dispatch, editorView);
+      items.forEach(({ command, dom, close }) => {
+        if (dom.contains(e.target)) {
+          command(editorView.state, editorView.dispatch);
+          if (close) close();
+        }
       });
     });
   }
 
   update() {
-    this.items.forEach(({ command, dom }) => {
-      let active = command(this.editorView.state, null, this.editorView);
-      // dom.style.display = active ? '' : 'none';
+    (this.items as { command: any; dom: HTMLElement }[]).forEach(({ command, dom }) => {
+      let active = command(this.editorView.state, null);
+      dom.setAttribute('aria-disabled', !active ? 'true' : 'false');
     });
   }
 
@@ -44,32 +49,60 @@ function menuPlugin(items) {
   });
 }
 
-function iconBold() {
-  let span = document.createElement('span');
-  span.setAttribute('role', 'button');
-  span.setAttribute('aria-pressed', 'false');
-  span.setAttribute('aria-label', 'Bold');
-  let innerSpan = document.createElement('span');
-  innerSpan.classList.add('icon', 'icon-bold');
-  span.appendChild(innerSpan);
-  return span;
-}
+export const createMenuPluginForContentTab = (mySchema) => {
+  const tb = document.querySelector('.content-tab.rich-text-editor-toolbar');
 
-function iconItalic() {
-  let span = document.createElement('span');
-  span.setAttribute('role', 'button');
-  span.setAttribute('aria-pressed', 'false');
-  span.setAttribute('aria-label', 'Italic');
-  let innerSpan = document.createElement('span');
-  innerSpan.classList.add('icon', 'icon-italic');
-  span.appendChild(innerSpan);
-  return span;
-}
-
-export const createMenuPlugin = (mySchema) => {
   let menu = menuPlugin([
-    { command: toggleMark(mySchema.marks.strong), dom: iconBold() },
-    { command: toggleMark(mySchema.marks.em), dom: iconItalic() },
+    /* bold */
+    { command: toggleMark(mySchema.marks.strong), dom: tb.children[0] },
+    /* italic */
+    { command: toggleMark(mySchema.marks.em), dom: tb.children[1] },
+    /* Heading 1 */
+    {
+      command: setBlockType(mySchema.nodes.heading, { level: 2 }),
+      dom: tb.children[4].querySelector('[role=menu]').children[0],
+      close: () => {
+        tb.children[4].removeAttribute('open');
+      },
+    },
+    /* Heading 2 */
+    {
+      command: setBlockType(mySchema.nodes.heading, { level: 3 }),
+      dom: tb.children[4].querySelector('[role=menu]').children[1],
+      close: () => {
+        tb.children[4].removeAttribute('open');
+      },
+    },
+    /* Heading 3 */
+    {
+      command: setBlockType(mySchema.nodes.heading, { level: 4 }),
+      dom: tb.children[4].querySelector('[role=menu]').children[2],
+      close: () => {
+        tb.children[4].removeAttribute('open');
+      },
+    },
+    /* Code Block */
+    { command: setBlockType(mySchema.nodes.code_block), dom: tb.children[5] },
+    /* Bullet List */
+    { command: wrapInList(mySchema.nodes.bullet_list), dom: tb.children[6] },
+    /* Number List */
+    { command: wrapInList(mySchema.nodes.ordered_list), dom: tb.children[7] },
+    /* Horizontal Rule */
+    {
+      command: (state, dispatch) => {
+        const available = true;
+        if (dispatch) dispatch(state.tr.replaceSelectionWith(mySchema.nodes.horizontal_rule.create()));
+        return available;
+      },
+      dom: tb.children[8],
+    },
+    /* Blockquote */
+    { command: wrapIn(mySchema.nodes.blockquote), dom: tb.children[9] },
+    /* Undo */
+    { command: undo, dom: tb.children[18].children[0] },
+    /* Redo */
+    { command: redo, dom: tb.children[18].children[1] },
   ]);
+
   return menu;
 };
