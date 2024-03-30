013b5cff7fe186ced563b0a8046633be1b155c0c
Jacques Desmarais <jdesma@gmail.com>
2024-03-30 10:03:33 -0400

    implements part of ThreadsController "create"

--- webapp/app/controllers/threads_controller.rb
+++ webapp/app/controllers/threads_controller.rb
@@ -2,15 +2,34 @@ class ThreadsController < ApplicationController
   deny_unauthenticated_access
 
   def create
+    _params = create_thread_params
+    permalink = _params["product_id"]
+    product = Product.find_by!(creator_id: Current.user.id, permalink: permalink)
+    init_openai_client
+    openaiResponse = @client.threads.create
+    thread = OpenaiAssistantThread.new(
+      product_id: product.id,
+      section: _params["section"],
+      thread_id: openaiResponse["id"],
+    )
+    thread.save!
+    respond_to do |format|
+      format.json { render json: { success: true } }
+    end
+  end
+
+  private
+
+  def init_openai_client
     OpenAI.configure do |config|
       config.access_token = ENV.fetch("OPENAI_ACCESS_TOKEN", "")
     end
     @client = OpenAI::Client.new
     @client = @client.beta(assistants: "v1")
-    openaiResponse = @client.threads.create
+  end
 
-    respond_to do |format|
-      format.json { render json: { success: true, thread: openaiResponse } }
-    end
+  def create_thread_params
+    params.require([:product_id, :section])
+    params.permit([:product_id, :section])
   end
 end
--- webapp/test/controllers/threads_controller_test.rb
+++ webapp/test/controllers/threads_controller_test.rb
@@ -8,20 +8,11 @@ class ThreadsControllerTest < ActionDispatch::IntegrationTest
     end
   end
 
-  test "create product thread" do
+  test "create product thread for product description" do
     sign_in :one
     VCR.use_cassette("create_thread") do
-      post product_threads_url(products(:p1).permalink), as: :json
+      post product_threads_url(products(:p1).permalink) + "?section=description", as: :json
     end
-    expectedResponse = {
-      "success" => true,
-      "thread" => {
-        "id" => "thread_92T6xfK8AWfd55aGiwLnS9MQ",
-        "object" => "thread",
-        "created_at" => 1711803386,
-        "metadata" => {},
-      },
-    }
-    assert_equal(expectedResponse, JSON.parse(@response.body))
+    assert_equal({ "success" => true }, JSON.parse(@response.body))
   end
 end
