813f5097baa0324b015723bb7a2cb3f6873dc23b
Jacques Desmarais <jdesma@gmail.com>
2024-03-30 09:01:14 -0400

    adds a 2nd VCR test for creating a thread using the OpenAI API

--- webapp/test/controllers/threads_controller_test.rb
+++ webapp/test/controllers/threads_controller_test.rb
@@ -1,6 +1,13 @@
 require "test_helper"
 
 class ThreadsControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    VCR.configure do |config|
+      config.cassette_library_dir = "test/fixtures-vcr/vcr_cassettes"
+      config.hook_into :webmock
+    end
+  end
+
   test "create product thread" do
     sign_in :one
     post product_threads_url(products(:p1).permalink), as: :json
@@ -9,14 +16,21 @@ class ThreadsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "vcr" do
-    VCR.configure do |config|
-      config.cassette_library_dir = "test/fixtures-vcr/vcr_cassettes"
-      config.hook_into :webmock
-    end
-
     VCR.use_cassette("synopsis") do
       response = Net::HTTP.get_response(URI("http://www.iana.org/domains/reserved"))
       assert_match /Example domains/, response.body
+    end
+  end
+
+  test "create thread in openai" do
+    VCR.use_cassette("create_thread") do
+      OpenAI.configure do |config|
+        config.access_token = ENV.fetch("OPENAI_ACCESS_TOKEN", "")
+      end
+      @client = OpenAI::Client.new
+      @client = @client.beta(assistants: "v1")
+      response = @client.threads.create
+      puts JSON.generate(response)
     end
   end
 end
