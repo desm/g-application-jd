b45dfe611b977588c5ae4ef0ad2bccb41435b1f6
Jacques Desmarais <jdesma@gmail.com>
2024-03-30 11:01:39 -0400

    updates "create thread" tests to pass section=description as POST payload instead

--- webapp/test/controllers/threads_controller_test.rb
+++ webapp/test/controllers/threads_controller_test.rb
@@ -12,7 +12,7 @@ class ThreadsControllerTest < ActionDispatch::IntegrationTest
   test "create product thread for product description when no thread exists" do
     VCR.use_cassette("create_thread") do
       # product "p2" has no thread for section "description" (see openai_assistant_threads.yml)
-      post product_threads_url(products(:p2).permalink) + "?section=description", as: :json
+      post product_threads_url(products(:p2).permalink), params: { section: "description" }, as: :json
     end
     assert_equal({ "success" => true }, JSON.parse(@response.body))
   end
@@ -20,7 +20,7 @@ class ThreadsControllerTest < ActionDispatch::IntegrationTest
   test "create product thread for product description when a thread for that product section already exists" do
     VCR.use_cassette("create_thread") do
       # product "p1" already has a thread for section "description" (see openai_assistant_threads.yml)
-      post product_threads_url(products(:p1).permalink) + "?section=description", as: :json
+      post product_threads_url(products(:p1).permalink), params: { section: "description" }, as: :json
     end
     assert_equal({ "success" => false }, JSON.parse(@response.body))
   end
