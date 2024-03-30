282a1f5cc3f2e9fd097a089dcfd55c32eb079a62
Jacques Desmarais <jdesma@gmail.com>
2024-03-30 06:00:25 -0400

    adds a first model test for OpenaiAssistantThread

--- webapp/test/models/openai_assistant_thread_test.rb
+++ webapp/test/models/openai_assistant_thread_test.rb
@@ -1,7 +1,12 @@
 require "test_helper"
 
 class OpenaiAssistantThreadTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "create a thread" do
+    thread = OpenaiAssistantThread.new(
+      product: products(:p1),
+      section: :description,
+      thread_id: "thread_003",
+    )
+    thread.save!
+  end
 end
