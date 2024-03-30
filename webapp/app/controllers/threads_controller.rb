725b2c71dc2d459c0db15fd63df4ee52de7d0271
Jacques Desmarais <jdesma@gmail.com>
2024-03-30 09:09:47 -0400

    starts implementing ThreadsController "create"

--- webapp/app/controllers/threads_controller.rb
+++ webapp/app/controllers/threads_controller.rb
@@ -2,8 +2,15 @@ class ThreadsController < ApplicationController
   deny_unauthenticated_access
 
   def create
+    OpenAI.configure do |config|
+      config.access_token = ENV.fetch("OPENAI_ACCESS_TOKEN", "")
+    end
+    @client = OpenAI::Client.new
+    @client = @client.beta(assistants: "v1")
+    openaiResponse = @client.threads.create
+
     respond_to do |format|
-      format.json { render json: { success: true } }
+      format.json { render json: { success: true, thread: openaiResponse } }
     end
   end
 end
--- webapp/test/controllers/threads_controller_test.rb
+++ webapp/test/controllers/threads_controller_test.rb
@@ -10,27 +10,18 @@ class ThreadsControllerTest < ActionDispatch::IntegrationTest
 
   test "create product thread" do
     sign_in :one
-    post product_threads_url(products(:p1).permalink), as: :json
-    assert_response :ok
-    assert_equal({ "success" => true }, JSON.parse(@response.body))
-  end
-
-  test "vcr" do
-    VCR.use_cassette("synopsis") do
-      response = Net::HTTP.get_response(URI("http://www.iana.org/domains/reserved"))
-      assert_match /Example domains/, response.body
-    end
-  end
-
-  test "create thread in openai" do
     VCR.use_cassette("create_thread") do
-      OpenAI.configure do |config|
-        config.access_token = ENV.fetch("OPENAI_ACCESS_TOKEN", "")
-      end
-      @client = OpenAI::Client.new
-      @client = @client.beta(assistants: "v1")
-      response = @client.threads.create
-      puts JSON.generate(response)
+      post product_threads_url(products(:p1).permalink), as: :json
     end
+    expectedResponse = {
+      "success" => true,
+      "thread" => {
+        "id" => "thread_92T6xfK8AWfd55aGiwLnS9MQ",
+        "object" => "thread",
+        "created_at" => 1711803386,
+        "metadata" => {},
+      },
+    }
+    assert_equal(expectedResponse, JSON.parse(@response.body))
   end
 end
