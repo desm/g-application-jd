2f84b3a7fd5468cc244d82fabee4c968a473ec3c
Jacques Desmarais <jdesma@gmail.com>
2024-04-01 11:16:20 -0400

    implements ThreadsController#update, which is backend to "Make Shorter" and "Make Longer" buttons

--- webapp/app/controllers/threads_controller.rb
+++ webapp/app/controllers/threads_controller.rb
@@ -32,11 +32,15 @@ class ThreadsController < ApplicationController
   def update
     Rails.error.set_context(
       section: "products",
-      action: "patch thread",
+      action: "use AI Assistant to rework text",
       user_email: Current.user.email_address,
+      params: params,
     )
 
-    success = false; status = 400
+    # request is bad until proven otherwise
+    success = false
+    status = 400
+    reworked_text = nil
 
     Rails.error.handle do
       _params = patch_thread_params
@@ -55,7 +59,12 @@ class ThreadsController < ApplicationController
         raise "unrecognized \"ask\" parameter"
       end
 
-      message = <<~TEXT
+      # server-side error unless all goes well (reproducing same 3 lines of code as above so it is easier to recognize)
+      success = false
+      status = 500
+      reworked_text = nil
+
+      openai_message_content = <<~TEXT
         This is the name and description of a digital product that is being sold on gumroad.com. Gumroad.com is an online platform that allows individual creators and businesses to sell their products directly to consumers. It supports a wide range of digital products, including but not limited to ebooks, software, music, and art assets like brush sets for digital painting programs.
 
         Product name: #{_params["product_name"]}
@@ -67,11 +76,65 @@ class ThreadsController < ApplicationController
         Your response should only contain the result of the rewrite.
       TEXT
 
-      success = true; status = 200
+      init_openai_client
+      response_of_message_create = @client.messages.create(
+        thread_id: thread.thread_id,
+        parameters: {
+          role: "user", # Required for manually created messages
+          content: openai_message_content,
+        },
+      )
+
+      message_id = response_of_message_create["id"]
+      thread.touch
+
+      response_of_runs_create = @client.runs.create(
+        thread_id: thread.thread_id,
+        parameters: {
+          assistant_id: "asst_WIrbrZvIEWPWyI4Y3XczBniu",
+        },
+      )
+      run_id = response_of_runs_create["id"]
+
+      while true
+        response = @client.runs.retrieve(id: run_id, thread_id: thread.thread_id)
+        status = response["status"]
+
+        case status
+        when "queued", "in_progress", "cancelling"
+          sleep 1 # Wait one second and poll again
+        when "completed"
+          break # Exit loop and report result to user
+        when "requires_action"
+          raise "Unexpected status of \"requires_action\" while poling for run completion"
+        when "cancelled", "failed", "expired"
+          raise response["last_error"].inspect
+        else
+          raise "Unknown status response: #{status}"
+        end
+      end
+
+      response_of_get_last_message = @client.get(path: "/threads/#{thread.thread_id}/messages?limit=1")
+
+      begin
+        reworked_text = response_of_get_last_message["data"][0]["content"][0]["text"]["value"]
+      rescue
+        raise "Unable to find reworked text in response: #{JSON.generate(response_of_get_last_message)}"
+      end
+
+      # all is well
+      success = true
+      status = 200
     end
 
-    respond_to do |format|
-      format.json { render json: { success: success }, status: status }
+    if success
+      respond_to do |format|
+        format.json { render json: { success: success, reworked_text: reworked_text }, status: status }
+      end
+    else
+      respond_to do |format|
+        format.json { render json: { success: success }, status: status }
+      end
     end
   end
 
--- webapp/test/controllers/threads_controller_test.rb
+++ webapp/test/controllers/threads_controller_test.rb
@@ -25,9 +25,8 @@ class ThreadsControllerTest < ActionDispatch::IntegrationTest
     assert_equal({ "success" => false }, JSON.parse(@response.body))
   end
 
-  # test "update product thread with request to make text shorter" do
-  test "A" do
-    VCR.use_cassette("update_thread") do
+  test "update product thread with request to make text SHORTER" do
+    VCR.use_cassette("ask_make_shorter") do
       patch product_thread_url(products(:p1).permalink, "description"),
             params: {
               product_name: "Foliage & Grass Brushset (Photoshop/Procreate)",
@@ -36,6 +35,27 @@ class ThreadsControllerTest < ActionDispatch::IntegrationTest
               ask: "ask to make selection a little bit shorter",
             }, as: :json
       assert_response :ok
+      assert_equal(
+        "These brushes were developed to accelerate my own foliage rendering process and assist others aiming to do the same. Also, I included a brush settings framework for artists to create original sets reflecting their unique styles.",
+        JSON.parse(@response.body)["reworked_text"]
+      )
+    end
+  end
+
+  test "update product thread with request to make text LONGER" do
+    VCR.use_cassette("ask_make_longer") do
+      patch product_thread_url(products(:p1).permalink, "description"),
+            params: {
+              product_name: "Foliage & Grass Brushset (Photoshop/Procreate)",
+              full_description: "So happy to release this set at long last! Tips and contributions welcome if you're able to and wish to!-Works with Photoshop CC and Procreate-125+ Brushes-3 hours of video content with full voiceover -Royalty free, use with any personal, school, or professional work. Remix, edit, and recreate brushes to your liking. -Due to popular request, transparent PNG files of the brush tips have been added.I created these brushes to speed up my own process for laying down foliage, as well as to help anyone else who's looking to do the same! I also hoped to provide a brush settings framework for anyone who wants to create a style-specific original set and copy the settings over. The video content is designed to explain the purpose of each brush and give a framework for how it might be used in a painting, as well as how to replicate the major brush archetypes used here.Special thanks to Matt Hubel who went through and perfected the brush settings for Procreate to match the PS set.",
+              selected_text: "I created these brushes to speed up my own process for laying down foliage, as well as to help anyone else who's looking to do the same! I also hoped to provide a brush settings framework for anyone who wants to create a style-specific original set and copy the settings over.",
+              ask: "ask to make selection a little bit longer",
+            }, as: :json
+      assert_response :ok
+      assert_equal(
+        "Born out of my desire to streamline the intricate process of illustrating foliage in my digital art, these brushes serve as a valuable tool not only to me, but also to fellow artists wishing to enhance their workflow. My intent was to accelerate both my personal work rate and provide a solution to those facing similar challenges in their artistic creation process. My aspiration goes beyond facilitating speed; I hoped to create a malleable brush settings framework that allows for the development of original, style-specific sets. It supports artists in tailoring the settings, empowering them to modify and reproduce the tools with ease, therefore linking their unique artistic finger prints to their creations. This amalgamation of efficiency and customization is a testament to the versatility I envisioned when creating this brushset.",
+        JSON.parse(@response.body)["reworked_text"]
+      )
     end
   end
 end
