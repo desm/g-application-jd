a43c637d74dfd405bf8ee3ff98dd69f97e859f70
Jacques Desmarais <jdesma@gmail.com>
2024-04-01 11:44:20 -0400

    refactors ThreadsController#update to make use of new AiAssistantService

--- webapp/app/controllers/threads_controller.rb
+++ webapp/app/controllers/threads_controller.rb
@@ -44,83 +44,38 @@ class ThreadsController < ApplicationController
 
     Rails.error.handle do
       _params = patch_thread_params
+
+      mode = nil
+      case _params["ask"]
+      when "ask to make selection a little bit shorter"
+        mode = :little_bit_shorter
+      when "ask to make selection a little bit longer"
+        mode = :little_bit_longer
+      else
+        raise "invalid \"ask\" parameter: #{_params["ask"]}"
+      end
+
       permalink = _params["product_id"]
       section = _params["id"] # the thread's section
       product = Product.includes(:openai_assistant_threads).find_by!(creator_id: Current.user.id, permalink: permalink)
       thread = product.openai_assistant_threads.where!(section: section).first
-
-      instruction = ""
-      case _params["ask"]
-      when "ask to make selection a little bit shorter"
-        instruction = "Make the following part a little bit shorter: #{_params["selected_text"]}"
-      when "ask to make selection a little bit longer"
-        instruction = "Make the following part a little bit longer: #{_params["selected_text"]}"
-      else
-        raise "unrecognized \"ask\" parameter"
-      end
 
       # server-side error unless all goes well (reproducing same 3 lines of code as above so it is easier to recognize)
       success = false
       status = 500
       reworked_text = nil
 
-      openai_message_content = <<~TEXT
-        This is the name and description of a digital product that is being sold on gumroad.com. Gumroad.com is an online platform that allows individual creators and businesses to sell their products directly to consumers. It supports a wide range of digital products, including but not limited to ebooks, software, music, and art assets like brush sets for digital painting programs.
-
-        Product name: #{_params["product_name"]}
-
-        Full product description: #{_params["full_description"]}
-
-        #{instruction}
-
-        Your response should only contain the result of the rewrite.
-      TEXT
-
       init_openai_client
-      response_of_message_create = @client.messages.create(
+      aiAssistantService = AiAssistantService.new(client: @client)
+      reworked_text = aiAssistantService.rework_product_description_selected_text(
         thread_id: thread.thread_id,
-        parameters: {
-          role: "user", # Required for manually created messages
-          content: openai_message_content,
-        },
+        mode: mode,
+        product_name: _params["product_name"],
+        full_description: _params["full_description"],
+        selected_text: _params["selected_text"],
       )
 
-      message_id = response_of_message_create["id"]
       thread.touch
-
-      response_of_runs_create = @client.runs.create(
-        thread_id: thread.thread_id,
-        parameters: {
-          assistant_id: "asst_WIrbrZvIEWPWyI4Y3XczBniu",
-        },
-      )
-      run_id = response_of_runs_create["id"]
-
-      while true
-        response = @client.runs.retrieve(id: run_id, thread_id: thread.thread_id)
-        status = response["status"]
-
-        case status
-        when "queued", "in_progress", "cancelling"
-          sleep 1 # Wait one second and poll again
-        when "completed"
-          break # Exit loop and report result to user
-        when "requires_action"
-          raise "Unexpected status of \"requires_action\" while poling for run completion"
-        when "cancelled", "failed", "expired"
-          raise response["last_error"].inspect
-        else
-          raise "Unknown status response: #{status}"
-        end
-      end
-
-      response_of_get_last_message = @client.get(path: "/threads/#{thread.thread_id}/messages?limit=1")
-
-      begin
-        reworked_text = response_of_get_last_message["data"][0]["content"][0]["text"]["value"]
-      rescue
-        raise "Unable to find reworked text in response: #{JSON.generate(response_of_get_last_message)}"
-      end
 
       # all is well
       success = true
---
+++ webapp/app/services/AiAssistantService.rb
@@ -0,0 +1,77 @@
+class AiAssistantService
+  def initialize(client:)
+    @client = client
+  end
+
+  def rework_product_description_selected_text(thread_id:, mode:, product_name:, full_description:, selected_text:)
+    reworked_text = nil
+
+    instruction = ""
+    case mode
+    when :little_bit_shorter
+      instruction = "Make the following part a little bit shorter: #{selected_text}"
+    when :little_bit_longer
+      instruction = "Make the following part a little bit longer: #{selected_text}"
+    else
+      raise "unrecognized mode: #{mode}"
+    end
+
+    openai_message_content = <<~TEXT
+      This is the name and description of a digital product that is being sold on gumroad.com. Gumroad.com is an online platform that allows individual creators and businesses to sell their products directly to consumers. It supports a wide range of digital products, including but not limited to ebooks, software, music, and art assets like brush sets for digital painting programs.
+
+      Product name: #{product_name}
+
+      Full product description: #{full_description}
+
+      #{instruction}
+
+      Your response should only contain the result of the rewrite.
+    TEXT
+
+    response_of_message_create = @client.messages.create(
+      thread_id: thread_id,
+      parameters: {
+        role: "user", # Required for manually created messages
+        content: openai_message_content,
+      },
+    )
+
+    message_id = response_of_message_create["id"]
+
+    response_of_runs_create = @client.runs.create(
+      thread_id: thread_id,
+      parameters: {
+        assistant_id: "asst_WIrbrZvIEWPWyI4Y3XczBniu",
+      },
+    )
+    run_id = response_of_runs_create["id"]
+
+    while true
+      response = @client.runs.retrieve(id: run_id, thread_id: thread_id)
+      status = response["status"]
+
+      case status
+      when "queued", "in_progress", "cancelling"
+        sleep 1 # Wait one second and poll again
+      when "completed"
+        break # Exit loop and report result to user
+      when "requires_action"
+        raise "Unexpected status of \"requires_action\" while poling for run completion"
+      when "cancelled", "failed", "expired"
+        raise response["last_error"].inspect
+      else
+        raise "Unknown status response: #{status}"
+      end
+    end
+
+    response_of_get_last_message = @client.get(path: "/threads/#{thread_id}/messages?limit=1")
+
+    begin
+      reworked_text = response_of_get_last_message["data"][0]["content"][0]["text"]["value"]
+    rescue
+      raise "Unable to find reworked text in response: #{JSON.generate(response_of_get_last_message)}"
+    end
+
+    reworked_text
+  end
+end
